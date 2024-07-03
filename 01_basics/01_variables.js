c81800bf963289357c711a5e615b4d3e406215d2
D. Sohan Kumar <119175471+sohans1092004@users.noreply.github.com>
2024-07-03 17:02:57 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword="12345"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2
+
+accountEmail="inkipinki@gmail.com"
+accountPassword="pomiki"
+accountCity="bglr"
+console.log(accountId);
+
+/* 
+    Prefer not to use var beacause of issue in block scope and functional scope.
+
+*/
+console.log(accountState)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//code will be treated as newer version of JS.
+
+// alert(3 + 2) // we are using node js, so its not working
+
+
+let name="sohan" //string
+let age=18 //number - upto 2^(53)
+let isLoggedIn = false//bool
+
+
+//Primitive Datatypes are - 
+//number upto 2 to power 53
+//bigint for numbers greater than 2 to power 53
+//string => ""
+//boolean
+//null => standalone value ->  it is different from undefined which means empty.
+//undefined => when you dont assign any value
+// symbol => unique
+
+
+//object
+
+//typeof - helps to find out what type of datatype it is
+console.log(typeof 4);
+console.log(typeof null);// gives o/p as object
+console.log(typeof undefined);// gives o/p as undefined
+
